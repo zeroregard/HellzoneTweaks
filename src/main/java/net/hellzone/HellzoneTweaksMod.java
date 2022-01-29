9f842c7500dc1ae982f6e4634eb2a382eb744114
Mathias <mathias_sn@hotmail.com>
2022-01-29 16:52:19 +0100

    added WIP magma sponge

--- src/main/java/net/hellzone/HellzoneTweaksMod.java
+++ src/main/java/net/hellzone/HellzoneTweaksMod.java
@@ -1,19 +1,49 @@
 package net.hellzone;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.hellzone.blocks.MagmaSpongeBlock;
+import net.hellzone.blocks.HotMagmaSpongeBlock;
 import net.hellzone.mixin.BrewingRecipeRegistryMixin;
 import net.hellzone.potions.HastePotion;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potions;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HellzoneTweaksMod implements ModInitializer {
+	public static final String MOD_ID = "hellzone";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Block MAGMA_SPONGE_BLOCK = new MagmaSpongeBlock(FabricBlockSettings.copy(Blocks.SPONGE), 6, 64);
+	public static final Block HOT_MAGMA_SPONGE_BLOCK = new HotMagmaSpongeBlock(FabricBlockSettings.copy(Blocks.SPONGE), MAGMA_SPONGE_BLOCK.getDefaultState());
 
 	@Override
 	public void onInitialize() {
-		var hastePotion = new HastePotion();
-		Registry.register(Registry.POTION, "hellzone:potion_of_haste", hastePotion);	
-		BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(Potions.AWKWARD, Items.AMETHYST_SHARD,hastePotion);
+		addHastePotionFeature();
+		addMagmaSpongeFeature();
+	}
 
+	private void addHastePotionFeature() {
+		var hastePotion = new HastePotion();
+		Registry.register(Registry.POTION, new Identifier(MOD_ID, "potion_of_haste"), hastePotion);	
+		BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(Potions.AWKWARD, Items.AMETHYST_SHARD,hastePotion);
+	}
+
+	private void addMagmaSpongeFeature() {
+		// add the blocks
+		((MagmaSpongeBlock)MAGMA_SPONGE_BLOCK).setHotSponge(HOT_MAGMA_SPONGE_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "magma_sponge"), MAGMA_SPONGE_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "hot_magma_sponge"), HOT_MAGMA_SPONGE_BLOCK);
+		// add the items
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "magma_sponge"), new BlockItem(MAGMA_SPONGE_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "hot_magma_sponge"), new BlockItem(HOT_MAGMA_SPONGE_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
 }
---
+++ src/main/java/net/hellzone/blocks/HotMagmaSpongeBlock.java
@@ -0,0 +1,91 @@
+// Copied from https://github.com/UltrusBot/ExtraSponges/blob/master/src/main/java/io/github/ultrusbot/extrasponges/block/HotLavaSpongeBlock.java
+
+package net.hellzone.blocks;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.hellzone.HellzoneTweaksMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class HotMagmaSpongeBlock extends Block {
+    private final BlockState originalBlock;
+    // public static final IntProperty CHARGES = IntProperty.of("charges", 0, 64);
+
+    public HotMagmaSpongeBlock(Settings settings, BlockState originalBlock) {
+        super(settings);
+        this.originalBlock = originalBlock;
+    }
+
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        boolean isTouchingWater = false;
+        for (BlockPos blockPos : BlockPos.iterateOutwards(pos, 1, 1, 1)) {
+            if (world.getBlockState(blockPos).getFluidState().isIn(FluidTags.WATER)) {
+                isTouchingWater = true;
+            }
+        }
+        if (isTouchingWater) {
+            world.setBlockState(pos, originalBlock, 3);
+            world.syncWorldEvent(2009, pos, 0);
+            world.playSound(null, pos, SoundEvents.BLOCK_LAVA_EXTINGUISH, SoundCategory.BLOCKS, 1.0F, (1.0F + world.getRandom().nextFloat() * 0.2F) * 0.7F);
+        }
+    }
+
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        // TODO
+        return ActionResult.PASS;
+     }
+
+    @Environment(EnvType.CLIENT)
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        Direction direction = Direction.random(random);
+        if (direction != Direction.UP) {
+            BlockPos blockPos = pos.offset(direction);
+            BlockState blockState = world.getBlockState(blockPos);
+            if (!state.isOpaque() || !blockState.isSideSolidFullSquare(world, blockPos, direction.getOpposite())) {
+                double d = pos.getX();
+                double e = pos.getY();
+                double f = pos.getZ();
+                if (direction == Direction.DOWN) {
+                    e -= 0.05D;
+                    d += random.nextDouble();
+                    f += random.nextDouble();
+                } else {
+                    e += random.nextDouble() * 0.8D;
+                    if (direction.getAxis() == Direction.Axis.X) {
+                        f += random.nextDouble();
+                        if (direction == Direction.EAST) {
+                            ++d;
+                        } else {
+                            d += 0.05D;
+                        }
+                    } else {
+                        d += random.nextDouble();
+                        if (direction == Direction.SOUTH) {
+                            ++f;
+                        } else {
+                            f += 0.05D;
+                        }
+                    }
+                }
+
+                world.addParticle(ParticleTypes.DRIPPING_LAVA, d, e, f, 0.0D, 0.0D, 0.0D);
+            }
+        }
+    }
+
+}
